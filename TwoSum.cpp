bbab5c3d5d1c996ab23e2fc8944fe7b0949a4f85
vita <77139117@qq.com>
2019-11-04 15:40:22 +0800

    TwoNums

---
+++ TwoSum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+  
+    vector<int> twoSum(vector<int>& nums, int target) {
+      unordered_map<int, int> mymap;
+      vector<int> result;
+      for(int i = 0; i < nums.size(); i++ ){
+        int temp = target - nums[i];
+        mymap.emplace(temp, i);
+      }  
+      
+      for(int i = 0; i < nums.size(); i++ ){
+        unordered_map<int, int>::const_iterator got = mymap.find(nums[i]);
+        if( got != mymap.end() && got->second != i ) {
+          result.push_back(i);
+          result.push_back(got->second);
+          return result;
+        }
+      }
+        /*/
+        vector<int> result;
+        for( int i = 0; i < nums.size(); i++ ) {
+            int temp = target - nums[i];
+            for( int j = 0; j < nums.size() && i != j; j++ ) {
+                if( temp == nums[j] ) {
+                    result.push_back(i);
+                    result.push_back(j);
+                }
+            }
+        }
+        //*/  
+      
+        /*/
+        for( auto i : nums ) {
+            vector<int>::iterator iter;
+            int temp = target - i;
+            for(iter = nums.begin(); iter != nums.end(); iter++ ) {
+                if( temp == *iter ) {
+                    result.push_back(i);
+                    result.push_back(target - i);
+                }
+            }
+        }
+        //*/
+       return result;
+    }
+};
